ab89b3f9da3f2e806a961496b80ca283c175961a
Clint <clintp1@gmail.com>
2015-03-24 08:46:16 +0000

    setup RuboCop  and Rake

---
+++ spec/feature/public_bike_access_spec.rb
@@ -0,0 +1,8 @@
+require 'capybara/rspec'
+feature 'member of the public access bike' do
+  scenario 'docking station releases a bike that is not broken' do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    expect(bike).not_to be_broken
+  end
+end
